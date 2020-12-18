ddeeeed763f189dcf4c69b1726725781e2efa5f3
abhishekd07 <abhishekd07@users.noreply.github.com>
2020-12-18 10:31:34 +0530

    Update Billing.java

--- JMS Code/src/Billing.java
+++ JMS Code/src/Billing.java
@@ -7,7 +7,7 @@ import java.io.*;
 class Billing extends JFrame implements ActionListener
 {
 
-//Simulating Changes to Code fhfghqweqwew
+//Simulating Changes to Code test
 	JInternalFrame iFrameBilling = new JInternalFrame("Add New Billing Item",true,true,true,true);
 	
 	Connection con;
